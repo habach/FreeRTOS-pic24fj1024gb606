339a078bc3b6d3d65b2b81d9726b0df094a347bb
hicl <hicl@hicl>
2020-07-02 18:47:24 +0700

    [Commit 0]

---
+++ Demo/PIC24_MPLAB/RTOSDemo_PIC24.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=RTOSDemo_PIC24.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/RTOSDemo_PIC24.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=rtosdemopic24.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/rtosdemopic24.x.tar
+# pic24fj1024gb606 configuration
+CND_ARTIFACT_DIR_pic24fj1024gb606=dist/pic24fj1024gb606/production
+CND_ARTIFACT_NAME_pic24fj1024gb606=RTOSDemo_PIC24.X.production.hex
+CND_ARTIFACT_PATH_pic24fj1024gb606=dist/pic24fj1024gb606/production/RTOSDemo_PIC24.X.production.hex
+CND_PACKAGE_DIR_pic24fj1024gb606=${CND_DISTDIR}/pic24fj1024gb606/package
+CND_PACKAGE_NAME_pic24fj1024gb606=rtosdemopic24.x.tar
+CND_PACKAGE_PATH_pic24fj1024gb606=${CND_DISTDIR}/pic24fj1024gb606/package/rtosdemopic24.x.tar
